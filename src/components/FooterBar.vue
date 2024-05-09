357155f754d30ef898ef83f1db535fa92bcb5fb4
shirochxyz <idunvoneinzbern@gmail.com>
2024-05-09 21:31:46 +0700

    fix footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -5,37 +5,45 @@
         <div class="wave wave3"></div>
         <div class="wave wave3"></div>
     </div>
-  <footer class="bg-[#222D56] text-white grid footer">
-      <!-- footer content -->
-    <div class="">
-        <div class="flex items-center mb-6">
-            <img src="../assets/osis-wing.png" alt="" class="w-14">
-            <h1 class="ml-4 ">OSIS SMK WIN-G</h1>
-        </div>
-        <div>
-            <a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M20.947 8.305a6.53 6.53 0 0 0-.419-2.216 4.61 4.61 0 0 0-2.633-2.633 6.606 6.606 0 0 0-2.186-.42c-.962-.043-1.267-.055-3.709-.055s-2.755 0-3.71.055a6.606 6.606 0 0 0-2.185.42 4.607 4.607 0 0 0-2.633 2.633 6.554 6.554 0 0 0-.419 2.185c-.043.963-.056 1.268-.056 3.71s0 2.754.056 3.71c.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.043 1.268.056 3.71.056s2.755 0 3.71-.056a6.59 6.59 0 0 0 2.186-.419 4.615 4.615 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.187.043-.962.056-1.267.056-3.71-.002-2.442-.002-2.752-.058-3.709zm-8.953 8.297c-2.554 0-4.623-2.069-4.623-4.623s2.069-4.623 4.623-4.623a4.623 4.623 0 0 1 0 9.246zm4.807-8.339a1.077 1.077 0 0 1-1.078-1.078 1.077 1.077 0 1 1 2.155 0c0 .596-.482 1.078-1.077 1.078z"></path><circle cx="11.994" cy="11.979" r="3.003"></circle></svg></a>
-            <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path fill-rule="evenodd" clip-rule="evenodd" d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"></path></svg></a>
-            <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M21.593 7.203a2.506 2.506 0 0 0-1.762-1.766C18.265 5.007 12 5 12 5s-6.264-.007-7.831.404a2.56 2.56 0 0 0-1.766 1.778c-.413 1.566-.417 4.814-.417 4.814s-.004 3.264.406 4.814c.23.857.905 1.534 1.763 1.765 1.582.43 7.83.437 7.83.437s6.265.007 7.831-.403a2.515 2.515 0 0 0 1.767-1.763c.414-1.565.417-4.812.417-4.812s.02-3.265-.407-4.831zM9.996 15.005l.005-6 5.207 3.005-5.212 2.995z"></path></svg></a>
-        </div>
-    </div>
+    
+    <footer class="bg-[#222D56] grid footer text-white ">
+        <!-- footer content -->
 
-    <div class="">
-        <h1 class="mb-6 py-4">Projects</h1>
-        <li> <a href="#">Lorem</a></li>
-        <li> <a href="#">Ipsum</a></li>
-        <li> <a href="#">Dolor</a></li>
-        <li> <a href="#">Sit Amet</a></li>
-    </div>
+            <div>
+                <div class="flex items-center mb-6">
+                    <img src="../assets/osis-wing.png" alt="" class="w-14">
+                    <h1 class="ml-4 ">OSIS SMK WIN-G</h1>
+                </div>
+                <p class="mb-6 text-sm">Jl. Raya Kalapannunggal km 07 Kp. Pamatutan Rt. 57/23 Desa Sundawenang, Kec. Parungkuda, Kab. Sukabumi, Jawa Barat 43357</p>
+                <div class="icon-content">
+                    <a href="#"><i class="bx bxl-instagram-alt"></i></a>
+                    <a href="#"><i class="bx bxl-github"></i></a>
+                    <a href="#"><i class="bx bxl-whatsapp"></i></a>
+                    <a href="#"><i class="bx bxl-youtube"></i></a>
+                </div>
+            </div>
+    
+            <div class="">
+                <h1 class="mb-6 py-4">Projects</h1>
+                <li> <a href="#">Lorem</a></li>
+                <li> <a href="#">Ipsum</a></li>
+                <li> <a href="#">Dolor</a></li>
+                <li> <a href="#">Sit Amet</a></li>
+            </div>
+    
+            <div>
+                <h1 class="mb-6 py-4">Quick Links</h1>
+                <li><router-link class="nav-menu" to="/">Home</router-link></li>
+                <li><router-link class="nav-menu" to="/sekbid">Sekbid</router-link></li>
+                <li><router-link class="nav-menu" to="/gallery">Gallery</router-link></li>
+                <li><router-link class="nav-menu" to="/about">About</router-link></li>
+            </div>
 
-    <div>
-        <h1 class="mb-6 py-4">Quick Links</h1>
-        <li><router-link class="nav-menu" to="/">Home</router-link></li>
-        <li><router-link class="nav-menu" to="/sekbid">Sekbid</router-link></li>
-        <li><router-link class="nav-menu" to="/gallery">Gallery</router-link></li>
-        <li><router-link class="nav-menu" to="/about">About</router-link></li>
-    </div>
+            <div class="copyright text-center text-xs">
+                <p><i class="bx bx-copyright"></i> Copyright <span class="font-semibold">OSIS SMK WIN-G</span> 2024</p>
+            </div>
+        </footer>
 
-  </footer>
 </template>
 <script>
 export default {
