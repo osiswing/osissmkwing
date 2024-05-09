f6729a10ddc98e17c42462bbe7b74a635f439ba9
shirochxyz <idunvoneinzbern@gmail.com>
2024-05-09 20:18:07 +0700

    new: Vercel Speed Insights

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+  import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
+
 <template>
   <div id="app">
+    <SpeedInsights />
     <router-view/>
   </div>
 </template>
